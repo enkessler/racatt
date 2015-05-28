0d2256b7374e808dbf7f7ed348d0e9b930505007
Eric Kessler <morrow748@gmail.com>
2015-05-28 02:09:32 -0400

    Rake task creation added.
    The module can now create convenient Rake tasks for running RSpec
    and Cucumber together.

--- lib/racatt.rb
+++ lib/racatt.rb
@@ -1,5 +1,47 @@
 require "racatt/version"
 
+require 'cucumber/rake/task'
+require 'rspec/core/rake_task'
+
+
 module Racatt
-  # Your code goes here...
+
+  extend Rake::DSL
+
+
+  def self.set_cucumber_options(options)
+    ENV['CUCUMBER_OPTS'] = options
+  end
+
+  def self.combine_options(set_1, set_2)
+    set_2 ? "#{set_1} #{set_2}" : set_1
+  end
+
+  def self.create_tasks
+    current_scope = Rake.application.current_scope.path
+
+    namespace 'cucumber' do
+      desc 'Run all of the Cucumber features'
+      task :features, [:command_options] do |_t, args|
+        set_cucumber_options(combine_options('-t ~@wip -t ~@off', args[:command_options]))
+      end
+      Cucumber::Rake::Task.new(:features)
+    end
+
+    namespace 'rspec' do
+      desc 'Run all of the RSpec specifications'
+      RSpec::Core::RakeTask.new(:specs, :command_options) do |t, args|
+        t.rspec_opts = "-t ~wip -t ~off "
+        t.rspec_opts << args[:command_options] if args[:command_options]
+      end
+    end
+
+    desc 'Test All The Things!'
+    task :test_everything, [:command_options] => [:clear_coverage] do |_t, args|
+      Rake::Task["#{current_scope}:rspec:specs"].invoke(args[:command_options])
+      Rake::Task["#{current_scope}:cucumber:features"].invoke(args[:command_options])
+    end
+
+  end
+
 end

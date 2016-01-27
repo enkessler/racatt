66e887c1ea0249a031cc210571ec4b8b45e0ba56
Eric Kessler <morrow748@gmail.com>
2016-01-26 21:06:04 -0500

    More flexible task arguments.
    Made it possible to provide separate arguments for the RSpec and
    Cucumber tasks and removed the default arguments that were previously
    used.

--- lib/racatt.rb
+++ lib/racatt.rb
@@ -13,17 +13,13 @@ module Racatt
     ENV['CUCUMBER_OPTS'] = options
   end
 
-  def self.combine_options(set_1, set_2)
-    set_2 ? "#{set_1} #{set_2}" : set_1
-  end
-
   def self.create_tasks
     current_scope = Rake.application.current_scope.path
 
     namespace 'cucumber' do
       desc 'Run all of the Cucumber features'
       task :features, [:command_options] do |_t, args|
-        set_cucumber_options(combine_options('-t ~@wip -t ~@off', args[:command_options]))
+        set_cucumber_options(args[:command_options]) if args[:command_options]
       end
       Cucumber::Rake::Task.new(:features)
     end
@@ -31,17 +27,16 @@ module Racatt
     namespace 'rspec' do
       desc 'Run all of the RSpec specifications'
       RSpec::Core::RakeTask.new(:specs, :command_options) do |t, args|
-        t.rspec_opts = "-t ~wip -t ~off "
-        t.rspec_opts << args[:command_options] if args[:command_options]
+        t.rspec_opts = args[:command_options] if args[:command_options]
       end
     end
 
     desc 'Test All The Things!'
-    task :test_everything, [:command_options] do |_t, args|
+    task :test_everything, [:rspec_command_options, :cucumber_command_options] do |_t, args|
       scope_string = current_scope.empty? ? '' : "#{current_scope}:"
 
-      Rake::Task["#{scope_string}rspec:specs"].invoke(args[:command_options])
-      Rake::Task["#{scope_string}cucumber:features"].invoke(args[:command_options])
+      Rake::Task["#{scope_string}rspec:specs"].invoke(args[:rspec_command_options])
+      Rake::Task["#{scope_string}cucumber:features"].invoke(args[:cucumber_command_options])
     end
 
   end
